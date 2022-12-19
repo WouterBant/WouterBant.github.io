f654619b54f5ad0361865289e0310b4bcbf7e162
WouterBant <99251015+WouterBant@users.noreply.github.com>
2022-12-19 20:05:20 +0100

    Update script.js

--- Code/script.js
+++ Code/script.js
@@ -1,5 +1,15 @@
 var icon = document.getElementById("icon");
 
+function load1(){
+    if(localStorage.getItem('dark-theme')) {
+        const back = localStorage.getItem('dark-theme');
+        if (back === "yes"){
+            document.body.classList.toggle("dark-theme");
+            icon.src = "../Media/moon.png";
+        }
+    }
+}
+
 icon.onclick = function() {
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
@@ -8,15 +18,5 @@ icon.onclick = function() {
     }else{
         localStorage.setItem('dark-theme',"no");
         icon.src = "../Media/sun.png";
-    }
-}
-
-function load1(){
-    if(localStorage.getItem('dark-theme')) {
-        const back = localStorage.getItem('dark-theme');
-        if (back === "yes"){
-            document.body.classList.toggle("dark-theme");
-            icon.src = "../Media/moon.png";
-        }
     }
 }
