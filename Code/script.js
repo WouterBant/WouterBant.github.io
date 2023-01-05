47cda7878e060de666fe911cae3972dc7e808c67
WouterBant <99251015+WouterBant@users.noreply.github.com>
2023-01-05 17:12:05 +0100

    go to top button added

--- Code/script.js
+++ Code/script.js
@@ -1,9 +1,9 @@
 var icon = document.getElementById("icon");
 
-function load1(){
-    if(localStorage.getItem('dark-theme')) {
+function load1() {
+    if (localStorage.getItem('dark-theme')) {
         const back = localStorage.getItem('dark-theme');
-        if (back === "yes"){
+        if (back === "yes") {
             document.body.classList.toggle("dark-theme");
             icon.src = "Media/moon.png";
         }
@@ -12,11 +12,29 @@ function load1(){
 
 icon.onclick = function() {
     document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
+    if (document.body.classList.contains("dark-theme")) {
         localStorage.setItem('dark-theme',"yes");
         icon.src = "Media/moon.png";
-    }else{
+    } else {
         localStorage.setItem('dark-theme',"no");
         icon.src = "Media/sun.png";
     }
+}
+
+
+let topButton = document.getElementById("top_button");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
